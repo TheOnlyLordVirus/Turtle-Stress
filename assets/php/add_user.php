0256bbbcc96049a1e6fce19b9d6b96103c121cb3
TheOnlyLordVirus <treahaxor@gmail.com>
2020-03-31 20:33:44 -0500

    More Small changes (Password censoring.)

--- assets/php/add_user.php
+++ assets/php/add_user.php
@@ -13,7 +13,7 @@
 
   $DATABASE_HOST = 'localhost';
   $DATABASE_USER = 'root';
-  $DATABASE_PASS = 'Kush007';
+  $DATABASE_PASS = 'MyPassWord';
   $DATABASE_NAME = 'USER_INFO_DB';
 
   // Try and connect using the info above.
